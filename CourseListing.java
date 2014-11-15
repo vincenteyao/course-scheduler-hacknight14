a4cc0f3a04219facc8b0b1b244be305414328547
darkknight123 <ashwin.srikant@gmail.com>
2014-11-14 21:40:12 -0800

    Added CourseListing.java

---
+++ CourseListing.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.*;
+/**
+ * 
+ * Name: CourseListing
+ * Purpose: Creates a Scanner that takes input and creates index values
+ * from what user provides
+ *
+ */
+public class CourseListing 
+{
+	public static int numberOfClasses = 0;
+	public static String courseID;
+	public static int numberOfDays;
+	public static int[] days;
+	public static int[] startHour;
+	public static int[] startMinute;
+	public static int[] length;
+	public static Course[] course;
+	
+	public static void main(String [] args)
+	{
+
+	    Scanner input = new Scanner( System.in ); 
+	    System.out.println("Enter number of classes you'd like to enter. Quit when done");
+	    while(input.hasNext())
+	    {
+	    	numberOfClasses = input.nextInt();
+	    }
+        course = new Course[numberOfClasses];
+	    for(int i = 0; i < numberOfClasses; i++)
+	    {
+	    	System.out.println("Enter course name");
+	    	courseID = input.nextLine();
+	    	System.out.println("Enter numbers of days course meets");
+	    	numberOfDays = input.nextInt();
+	    	System.out.println("Enter course day. Monday = 1 - Friday = 5");
+	    	while(input.hasNext())
+	    	{
+	    		for(int j = 0; j < numberOfDays; j++){
+	    		days[j] = input.nextInt();
+		    	System.out.println("Enter course start time - hour");
+		    	startHour[j] = input.nextInt();
+		    	System.out.println("Enter course start time - minute");
+		    	startMinute[j] = input.nextInt();
+		    	System.out.println("Enter course length in minutes");
+		    	length[j] = input.nextInt();
+	    		}	    		}
+	    	}
+
+	    for(int k = 0; k < numberOfClasses; k++)
+	    {
+	    	course[k] = new Course(courseID, days, startHour, startMinute, length);	    	
+	    }
+	}
+}
+
